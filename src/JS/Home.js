176eb681e3aab0676a49f887259dbe739ba7a6a7
Gazdag Marcell <gmarcell2007@gmail.com>
2021-09-10 19:21:57 +0200

    Automatic commit

--- src/JS/Home.js
+++ src/JS/Home.js
@@ -1,12 +1,35 @@
 import '../CSS/Test.css';
 import { useHistory } from 'react-router-dom';
+import React, {useState} from 'react'
 
 export default () => {
+    const [data, setData] = useState(null);
+
+    function getData(val) {
+        setData(val.target.value);
+    }
+    
     const history = useHistory();
+    var wsUri = "ws://sportnaplo.herokuapp.com/";
+    var websocket;
 
     return (
-        <div class="test">
+        <div className="test">
             <button onClick={() => history.push('/test')}>Test</button>
+            <input type="text" onChange={getData}></input>
+            <button onClick={() => send(data)}>send</button>
         </div>
     );
+
+    function send(message) {
+        websocket = new WebSocket(wsUri);
+
+        websocket.onopen = function (e) {
+            websocket.send(message);
+        };
+
+        websocket.onerror = function (e) {
+            console.log("ERROR: " + e.data);
+        };
+    }
 }
--- src/JS/Template.js
+++ src/JS/Template.js
@@ -1,12 +1,21 @@
 import '../CSS/Test.css';
 import { useHistory } from 'react-router-dom';
+import React, {useState} from 'react'
 
 export default () => {
+    const [data, setData] = useState(null);
+
+    function getData(val) {
+        setData(val.target.value);
+    }
+
     const history = useHistory();
 
     return (
-        <div class="test">
+        <div className="test">
             <button onClick={() => history.push('/')}>Home</button>
+            <h1>{data}</h1>
+            <input type="text" onChange={getData}></input>
         </div>
     );
 }
