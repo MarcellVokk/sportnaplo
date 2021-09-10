8fc84dbd7125619f794015c330e0114c402c925d
Gazdag Marcell <gmarcell2007@gmail.com>
2021-09-10 20:02:39 +0200

    Automatic commit

--- src/JS/Home.js
+++ src/JS/Home.js
@@ -14,6 +14,7 @@ export default () => {
 
     return (
         <div className="test">
+            <a>Hello 1</a>
             <button onClick={() => history.push('/test')}>Test</button>
             <input type="text" onChange={getData}></input>
             <button onClick={() => send(data)}>send</button>
